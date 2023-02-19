cbbb05913f8ed995eadc00fdecdcdaaac66e8098
ummarat <umm.marat@gmail.com>
2023-02-19 16:21:17 +0200

    Delete test.sql

--- JDBC1_work2/src/test.sql
+++
@@ -1,3 +0,0 @@
-create database testdb;
-use testdb;
-CREATE TABLE arhiv (id int auto_increment primary key not null, fond int (25) not null, opis int (25) not null, sprava int (25) not null, name varchar (200) not null, stor int (100) not null);
